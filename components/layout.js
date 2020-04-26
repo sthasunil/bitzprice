865b5a2e0372239527bea8366fdbc48c0c0612bf
unknown <mail.sunilshrestha@gmail.com>
2020-04-26 14:58:18 -0400

    filename has to match casing

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 const Layout = (props) => (
     <div>
         <Head>
-            <title>BitzPrice</title>
+            <title>BitzPrices</title>
             <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css" />
         </Head>
         <Navbar />
